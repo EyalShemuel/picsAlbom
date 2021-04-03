f9e1762c7cefa26b9e8f15e3e3564e3875094320
eyal home <eshemuel1@gmail.com>
2021-04-03 09:31:57 +0300

    1

--- client/src/features/Users/AddUser.jsx
+++ client/src/features/Users/AddUser.jsx
@@ -1,14 +1,21 @@
-import React,{useState} from 'react'
-import {useDispatch} from 'react-redux'
-import {addUser} from './UsersSlice'
-import {nanoid} from '@reduxjs/toolkit'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addUser } from './UsersSlice';
+import { nanoid } from '@reduxjs/toolkit';
 
 function AddUser() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const dispatch = useDispatch();
+  const [userName, setUserName] = useState('');
+  dispatch(addUser({ id: nanoid(), user: text, userPic: text }));
+
+  return (
+    <div className="add-user">
+        <h1>Register section</h1>
+      <p>please enter user Name</p>
+      <input value={userName} onChange={(e)=> setUserName(e.target.value)}/>
+    
+    </div>
+  );
 }
 
-export default AddUser
+export default AddUser;
