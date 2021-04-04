cae0e8ee00ba3f94a817103bb0d1b971f064a148
eyal home <eshemuel1@gmail.com>
2021-04-04 08:50:51 +0300

    1

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,37 @@
 import React from 'react';
-/* import logo from './logo.svg';
-import { Counter } from './features/counter/Counter'; */
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Button from './features/baisic/Button';
 import Image from './features/baisic/Image';
+import AddUser from './features/Users/AddUser';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Image 
+    <Router>
+      <Switch>
+        <Route path="/AddUser">
+          <AddUser />
+        </Route>
+        {/* <Route path="/Cat">
+            <Cat />
+          </Route> 
+          <Route path="/">
+            <Cars />
+          </Route> */}
+      </Switch>
+      <div className="App">
+        <header className="App-header">
+          {/* <Image 
         src='https://www.focus2move.com/wp-content/uploads/2020/01/Tesla-Roadster-2020-1024-03-696x522.jpg.webp'
-        size='S'/>
-      <Button 
-      value = "+"
-      command = 'increment()'
-      />
-      </header>
-    </div>
+        size='S'/> */}
+         
+            <Link to="/AddUser" style={{textDecoration:"none"}}> <Button value="+" /></Link> 
+         
+        </header>
+      </div>
+
+      
+    </Router>
   );
 }
 
