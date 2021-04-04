a6a6b2ea2dfffe11547f6a7f186376dbf803b28f
eyal home <eshemuel1@gmail.com>
2021-04-04 09:27:42 +0300

    1

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,26 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Button from './features/baisic/Button';
-import Image from './features/baisic/Image';
+import AlbomPage from './features/Albom/AlbomPage';
+
 import AddUser from './features/Users/AddUser';
 
 function App() {
   return (
     <Router>
+     
+      <div className="App">
       <Switch>
         <Route path="/AddUser">
           <AddUser />
         </Route>
-        {/* <Route path="/Cat">
-            <Cat />
+        <Route path="/Albom">
+            <AlbomPage />
           </Route> 
-          <Route path="/">
+      {/*     <Route path="/">
             <Cars />
           </Route> */}
       </Switch>
-      <div className="App">
         <header className="App-header">
           {/* <Image 
         src='https://www.focus2move.com/wp-content/uploads/2020/01/Tesla-Roadster-2020-1024-03-696x522.jpg.webp'
--- client/src/features/Users/Users.js
+++ client/src/features/Users/Users.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Image from './features/baisic/Image';
 
-const Users = () => {
+const Users = (props) => {
     return (
         <div>
-            
+           <Image 
+        src={props.src}
+        size='S'/>  
         </div>
     )
 }
